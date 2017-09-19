8aefa4d798795367264d4d33caa678626dbf05bd
Novik <novik65@gmail.com>
2017-09-19 19:16:34 +0300

    work in progress

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.updateParticipantsCount = functions.database.ref('/activities/{activityId}/participants/{participantId}').onWrite(event => 
+exports.updateActivityParticipantsCount = functions.database.ref('/activities/{activityId}/participants/{participantId}').onWrite(event => 
 {
 	if(event.data.exists())
 	{
@@ -34,6 +34,29 @@ exports.updateParticipantsCount = functions.database.ref('/activities/{activityI
 			  				participants: count
 						}));
 					}
+				}));
+			}));
+	}
+});
+
+exports.updateOrganizationParticipantsCount = functions.database.ref('/organizationDetails/{organizationId}/participants/{participantId}').onWrite(event => 
+{
+	if(event.data.exists())
+	{
+		return( 
+			admin.database().ref('/organizationDetails/'+event.params.organizationId+'/participants').once("value").then( function(snap) 
+	  		{
+	  			let count = 0;
+				snap.forEach(function(child) 
+				{
+	  				if( child.val().signed )
+	  		  		{
+	  		  			count++;
+		  		  	}
+		  		});
+	  			return( admin.database().ref('/organizations/'+event.params.organizationId).update(
+	  			{
+	  				participantCount: count
 				}));
 			}));
 	}
